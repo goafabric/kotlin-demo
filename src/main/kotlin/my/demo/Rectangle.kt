7e739e4e16a38d2603e2f913100866b04c559100
Andreas Mautsch <amautsch>
2021-07-09 10:28:27 +0200

    changes

--- src/main/kotlin/my/demo/Rectangle.kt
+++ src/main/kotlin/my/demo/Rectangle.kt
@@ -1,11 +1,11 @@
 package my.demo
 
+import java.util.concurrent.atomic.AtomicLong
+
 //by indicating the constructor params as val, they become automatic instance variables
 //and they are automatically initialized  with the constructor
 class Rectangle(val height: Double, private val length: Double) {
     val perimeter = calcPerimeter(height, length)
-
-    lateinit var duration : String
 
     private fun calcPerimeter (height: Double, length: Double) : Double {
         return (height + length) * 2
--- src/main/kotlin/my/demo/RectangleNullable.kt
+++ src/main/kotlin/my/demo/RectangleNullable.kt
@@ -1,11 +1,16 @@
 package my.demo
 
+import java.util.concurrent.atomic.AtomicLong
+
 class RectangleNullable(val height: Double, private val length: Double?) {
     val perimeter = calcPerimeter(height, length)
 
     constructor(height: Double) : this(height, 2.0) //secondary constructor
 
+    lateinit var duration : AtomicLong //mutable instances must be delclared explictly
+
     private fun calcPerimeter (height: Double, length: Double?) : Double {
+        duration.set(0L);
         return (height + length!!) * 2
     }
 
